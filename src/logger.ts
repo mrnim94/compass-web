5dacdfdcddccc43679f83571d1d10cb66e4b0a2c
Haohan Yang <yanghaohanmail@gmail.com>
2025-01-06 22:00:23 +0100

    Bug fix

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,12 @@
 export const Logger = {
   log: (name: string, component: string, ...args: any[]) => {
-    console.log(name, component, args);
+    if (process.env.NODE_ENV === 'development') {
+      console.log(name, component, args)
+    }
   },
 
   debug: (name: string, component: string, ...args: any[]) => {
-    if (process.env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV === 'development') {
       console.log(name, component, args);
     }
   },
--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,6 +1,6 @@
 export const Telemetry = {
   track: (event: string, properties: any) => {
-    if (process.env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV === 'development') {
       console.log(event, properties);
     }
   },
